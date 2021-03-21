52b3eb72bdfd2d637801ad7b75628596809bd918
ypaulsen <55412133+ypaulsen@users.noreply.github.com>
2021-03-20 23:29:42 -0700

    Create FizzBuzz.R

---
+++ FizzBuzz.R
@@ -0,0 +1,41 @@
+
+
+FizzBuzz1 <- c()
+
+for(i in 1:100){
+  FizzBuzz1[i] <- i 
+  if (i%%5 == 0 & i%%3 == 0) {FizzBuzz1[i] <- "Fizz Buzz"}
+  else { 
+    if (i%%3 == 0) {FizzBuzz1[i] <- "Fizz"} 
+    else{
+      if (i%%5 == 0) {FizzBuzz1[i] <- "Buzz"}
+    }}
+}
+FizzBuzz1
+
+
+FizzBuzz2 <- 1:100
+fz <- which(FizzBuzz2%%3 == 0)
+bz <- which(FizzBuzz2%%5 == 0)
+fzbz <- which(FizzBuzz2%%5 == 0 & FizzBuzz2%%3 == 0)
+FizzBuzz2[fz] <- "Fizz"
+FizzBuzz2[bz] <- "Buzz"
+FizzBuzz2[fzbz] <- "Fizz Buzz"
+FizzBuzz2
+
+
+FizzBuzz3 <- 1:100
+x <- seq(0, 100, by=3)[-1]
+y <- seq(0, 100, by=5)[-1]
+z <- seq(0, 100, by=15)[-1]
+FizzBuzz3[x] <- "Fizz"
+FizzBuzz3[y] <- "Buzz"
+FizzBuzz3[z] <- "Fizz Buzz"
+FizzBuzz3
+
+
+FizzBuzz1 == FizzBuzz2
+
+FizzBuzz2 == FizzBuzz3
+
+FizzBuzz1 == FizzBuzz3
